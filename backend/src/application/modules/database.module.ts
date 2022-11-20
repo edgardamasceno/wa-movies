4d7bf69993a19289fa091e0363fe8d3d2a47aaa5
Edgar Damasceno <edgar@Edgars-MacBook-Pro.local>
2022-11-20 06:01:43 -0300

    feat(wipe database): create wipe movie database endpoint

--- backend/src/application/modules/database.module.ts
+++ backend/src/application/modules/database.module.ts
@@ -4,10 +4,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Movie } from '../domain/entities/movie.entity';
 import { UpdateMovieDatabaseUsecaseService } from '../domain/usecases/update-movie-database.usecase.service';
 import { DatabaseController } from '../infraestructure/rest/database.controller';
+import { WipeMovieDatabaseUsecaseService } from '../domain/usecases/wipe-movie-database.usecase.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Movie]), HttpModule],
   controllers: [DatabaseController],
-  providers: [UpdateMovieDatabaseUsecaseService],
+  providers: [
+    UpdateMovieDatabaseUsecaseService,
+    WipeMovieDatabaseUsecaseService,
+  ],
 })
 export class DatabaseModule {}
